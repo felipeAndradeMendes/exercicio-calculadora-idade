e8057136cc2dc885861be8c97dba4f6217fe472b
Felipe Mendes <felipemadruga@gmail.com>
2023-10-02 18:13:01 -0300

    feat(req 01): implementa controller e service de calculo de idade

--- src/main/java/com/betrybe/calcuradoradeidade/controller/AgeCalculatorController.java
+++ src/main/java/com/betrybe/calcuradoradeidade/controller/AgeCalculatorController.java
@@ -1,5 +1,40 @@
 package com.betrybe.calcuradoradeidade.controller;
 
-public class AgeCalculatorController {
+import com.betrybe.calcuradoradeidade.dto.DateDto;
+import com.betrybe.calcuradoradeidade.service.AgeCalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Classe controller de idade.
+ */
+@RestController
+@RequestMapping("/calculateAge")
+public class AgeCalculatorController implements AgeCalculatorControllerInterface {
+  AgeCalculatorService service;
+
+  @Autowired
+  public AgeCalculatorController(AgeCalculatorService service) {
+    this.service = service;
+  }
+
+  @Override
+  @GetMapping()
+  public ResponseEntity<DateDto> calculateAge(
+      @RequestParam String date,
+      @RequestParam(defaultValue = "0") String orDefaultAge) {
+    System.out.println("entrei no controller");
+    int age = service.calculateAge(date);
+    System.out.println(age);
+    return ResponseEntity.ok(new DateDto(age));
+  }
+
+  @GetMapping("/testando")
+  public String testando() {
+    return "Deu certo essa rota";
+  }
 }
--- src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
+++ src/main/java/com/betrybe/calcuradoradeidade/service/AgeCalculatorService.java
@@ -1,13 +1,26 @@
 package com.betrybe.calcuradoradeidade.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import org.springframework.stereotype.Service;
+
 /**
  * Aqui nessa classe devem ser implementados os métodos para atender aos requisitos do exercício,
  * sinta-se à vontade para criar métodos privados para te auxiliar nas validações.
  */
+@Service
 public class AgeCalculatorService {
+
+  /**
+   * Metodo de calculo da idade.
+   *
+   * @param date data de nascimento.
+   * @return inteiro de anos.
+   */
   public int calculateAge(String date) {
-    // TODO method implementation
-    return -1;
+    LocalDate currentDate = LocalDate.now();
+    LocalDate birthDate = LocalDate.parse(date);
+    return Period.between(birthDate, currentDate).getYears();
   }
 
   public int calculateAgeWithDefault(String date, int defaultAge) {
