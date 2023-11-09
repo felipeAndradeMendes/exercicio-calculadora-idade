906cffea7a92bc411bb562c7b487afe7eec2d24c
Felipe Mendes <felipemadruga@gmail.com>
2023-11-09 11:50:52 -0300

    commit pra subir

---
+++ src/main/java/com/betrybe/calcuradoradeidade/Principal.java
@@ -0,0 +1,17 @@
+//package com.betrybe.calcuradoradeidade;
+//
+//import com.betrybe.calcuradoradeidade.controller.AgeCalculatorController;
+//
+//public class Principal {
+//  static AgeCalculatorController controller;
+//
+//  public Principal(AgeCalculatorController controller) {
+//    this.controller = controller;
+//  }
+//
+//  public static void main(String[] args) {
+//    controller.calculateAge("2000-01-01", 0);
+//
+//  }
+//
+//}
